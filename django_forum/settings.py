e78c5ceb0be136e5e266d476155e0d4cf5616546
Yadira Rodriguez <yadiraolisday99@gmail.com>
2022-08-29 22:18:47 -0400

    staticfile added

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -122,16 +122,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/staic/'
 
 cloudinary.config(
     cloud_name="drfyzg0or",
